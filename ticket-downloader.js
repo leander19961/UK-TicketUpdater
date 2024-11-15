4fc6f94c294225f6a18c5c5464b143fba97b0392
leander19961 <44499274+leander19961@users.noreply.github.com>
2024-11-15 10:27:19 +0100

    ticket-downloader.js

--- ticket-downloader.js
+++ ticket-downloader.js
@@ -15,31 +15,62 @@ const path = require('path');
   await page.waitForSelector('button[type="submit"]');
   await page.click('button[type="submit"]');
 
-  // reopen ticket-website
-  const page2 = await browser.newPage();
-  await page2.goto('https://ticket.astakassel.de');
+  // initialize output variable
+  let html = '';
 
-  // check if puppeteer got redirected to "gtc-site"
-  const textExists = await page2.evaluate(() => {
+  try {
+    // reopen ticket-website
+    const page2 = await browser.newPage();
+    await page2.goto('https://ticket.astakassel.de');
+    
+    // check if puppeteer got redirected to "privacy policy-site"
+    const privacyTextExists = await page2.evaluate(() => {
     return document.body.textContent.includes('Website of the semester ticket');
-  });
+    });
 
-  // click on accept on "gtc-site"
-  if (textExists) {
-    await page2.waitForSelector('input[type="submit"][value="Accept"]');
-    await page2.click('input[type="submit"][value="Accept"]');
+    // check if puppeteer got redirected to "ticket-site"
+    const ticketTextExists = await page2.evaluate(() => {
+    return document.body.textContent.includes('NVV-Semesterticket');
+    });
+    
+    if (ticketTextExists) {
+      // download ticket-html
+      html = await page2.content();
+    } else if (privacyTextExists) {
+      // click on accept on "privacy policy-site"
+      await page2.waitForSelector('input[type="submit"][value="Accept"]');
+      await page2.click('input[type="submit"][value="Accept"]');
+      
+      // reopen ticket-website
+      const page3 = await browser.newPage();
+      await page3.goto('https://ticket.astakassel.de');
+      
+      // download ticket-html
+      html = await page3.content();
+    } else {
+      // generate error html
+      html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Error</title>
+        </head>
+        <body>
+          <h1>Error</h1>
+          <p>Error downloading ticket...</p>
+        </body>
+        </html>
+      `;
+    }
+    
+    // save html in file
+    const filePath = path.resolve('/Path/To/File', 'Filename.html');
+    fs.writeFileSync(filePath, html);
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
-
-  // reopen ticket-website
-  const page3 = await browser.newPage();
-  await page3.goto('https://ticket.astakassel.de');
-
-  // download ticket-html
-  const html = await page3.content();
-
-  // save html in file
-  const filePath = path.resolve('/Path/To/File', 'Filename.html');
-  fs.writeFileSync(filePath, html);
 
   await browser.close();
 })();
